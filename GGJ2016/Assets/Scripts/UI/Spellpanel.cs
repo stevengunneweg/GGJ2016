8304d2dc837cb57ff951ec3a7da5c5c857bf4eac
Goosfrabix <goosfraba05@hotmail.com>
2016-01-29 21:20:25 +0100

    added UI buttons and GeneralManager

--- GGJ2016/Assets/Scripts/UI/Spellpanel.cs
+++ GGJ2016/Assets/Scripts/UI/Spellpanel.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GeneralManager : MonoBehaviour {
+public class Spellpanel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
