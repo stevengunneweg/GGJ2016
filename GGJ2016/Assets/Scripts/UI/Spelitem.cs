450e1d4a1a99e0f298399aad74c639a6ce1c348c
Goosfrabix <goosfraba05@hotmail.com>
2016-01-29 23:25:35 +0100

    spellitem pass along element and gets added as spell

---
+++ GGJ2016/Assets/Scripts/UI/Spelitem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spelitem : MonoBehaviour {
+    public Element element;
+    private Spellpanel _spellPanel;
+	// Use this for initialization
+	void Start () {
+        _spellPanel = transform.parent.GetComponent<Spellpanel>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnClick()
+    {
+        _spellPanel.AddElement(element);
+    }
+}
--- GGJ2016/Assets/Scripts/UI/Spellpanel.cs
+++ GGJ2016/Assets/Scripts/UI/Spellpanel.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spellpanel : MonoBehaviour {
 
-    public Element[] elements;
+    private List<Element> _spell = new List<Element>();
+
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +14,23 @@ public class Spellpanel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendSpell();
+        }
 	}
-    public void SpellSelected()
+    public void AddElement(Element value)
+    {
+        _spell.Add(value);
+        string line = "";
+        foreach(Element el in _spell)
+        {
+            line += el.name + ", ";
+        }
+
+        Debug.Log("Spell: "+ _spell.Count+" Content: "+ line);
+    }
+    void SendSpell()
     {
 
     }
