8809f6258487577671e5da6c1f4146788fd0613c
Goosfrabix <goosfraba05@hotmail.com>
2016-01-29 22:02:42 +0100

    added SpellManager

---
+++ GGJ2016/Assets/Scripts/Managers/SpellManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellManager : MonoBehaviour {
+
+    Spell[] _spells;
+    Element[] _elements;
+
+    // Use this for initialization
+    void Start () {
+        _spells = Resources.FindObjectsOfTypeAll<Spell>();
+        _elements = Resources.FindObjectsOfTypeAll<Element>();
+        Debug.Log("Spells "+_spells.Length);
+        Debug.Log("Elements " + _elements.Length);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
