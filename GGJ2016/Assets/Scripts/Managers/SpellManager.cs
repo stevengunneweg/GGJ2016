4ea808f5f27baff0159cbca603a43b01fef37b10
Goosfrabix <goosfraba05@hotmail.com>
2016-01-29 22:58:16 +0100

    Chenged Data map to Resources and SpellManager collects all spells and Elements;

--- GGJ2016/Assets/Scripts/Managers/SpellManager.cs
+++ GGJ2016/Assets/Scripts/Managers/SpellManager.cs
@@ -3,19 +3,48 @@ using System.Collections;
 
 public class SpellManager : MonoBehaviour {
 
+    Object[] _spellsObjects;
+    Object[] _elementsObjects;
     Spell[] _spells;
     Element[] _elements;
 
     // Use this for initialization
     void Start () {
-        _spells = Resources.FindObjectsOfTypeAll<Spell>();
-        _elements = Resources.FindObjectsOfTypeAll<Element>();
-        Debug.Log("Spells "+_spells.Length);
-        Debug.Log("Elements " + _elements.Length);
+        SetSpellAndElements();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
+    void SetSpellAndElements()
+    {
+        //_spells = Resources.FindObjectsOfTypeAll<Spell>();
+        //_elements = Resources.FindObjectsOfTypeAll<Element>();
+        _spellsObjects = Resources.LoadAll("Data", typeof(Spell));
+        _elementsObjects = Resources.LoadAll("Data", typeof(Element));
+        _spells = new Spell[_spellsObjects.Length];
+        _elements = new Element[_elementsObjects.Length];
+        for (int i=0;i< _spellsObjects.Length;i++)
+        {
+            _spells[i] = _spellsObjects[i] as Spell;
+        }
+        for (int i = 0; i < _spellsObjects.Length; i++)
+        {
+            _elements[i] = _elementsObjects[i] as Element;
+        }
+        Debug.Log("_spells: " + _spells.Length+" 0:"+ _spells[0]);
+        Debug.Log("_elements: " + _elements.Length + " 0:" + _elements[0]);
+    }
+
+    public Spell[] Spells
+    {
+        get { return _spells; }
+    }
+    public Element[] Elements
+    {
+        get { return _elements; }
+    }
+
 }
