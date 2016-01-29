494894cc1881aa6d0f60dc6e25fd4054ca96778f
Goosfrabix <goosfraba05@hotmail.com>
2016-01-29 21:40:02 +0100

    shit i dunno

--- GGJ2016/Assets/Scripts/UI/Spellpanel.cs
+++ GGJ2016/Assets/Scripts/UI/Spellpanel.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Spellpanel : MonoBehaviour {
 
+    public Element[] elements;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    public void SpellSelected()
+    {
+
+    }
 }
