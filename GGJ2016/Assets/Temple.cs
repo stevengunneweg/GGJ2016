d3453a1f2a8ec7c77540ebef1e611f0f3f83727c
Bas Roding <bas.roding@gmail.com>
2016-01-29 22:58:39 +0100

    Added new camera stuff

---
+++ GGJ2016/Assets/Temple.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Temple : MonoBehaviour {
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            RaiseTemple();
+        }else if(Input.GetKeyDown(KeyCode.DownArrow)){
+            LowerTemple();
+        }
+    }
+
+    public void RaiseTemple(){
+        LeanTween.moveLocalY(gameObject, transform.localPosition.y + 1.6f, 1).setEase(LeanTweenType.easeOutQuad);
+        ShakeCamera();
+    }
+
+    public void LowerTemple(){
+        LeanTween.moveLocalY(gameObject, transform.localPosition.y - 1.6f, 1).setEase(LeanTweenType.easeOutQuad);
+        ShakeCamera();
+    }
+
+    private void ShakeCamera(){
+        Camera.main.GetComponent<CameraShaker>().Shake(0.03f, 0.8f);
+    }
+}
