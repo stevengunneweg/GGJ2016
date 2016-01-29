503edf03589efcee1e17d137bbcb86975705e560
Goosfrabix <goosfraba05@hotmail.com>
2016-01-29 23:45:06 +0100

    ask for spell and send spell to eventManager

--- GGJ2016/Assets/Scripts/UI/Spellpanel.cs
+++ GGJ2016/Assets/Scripts/UI/Spellpanel.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 
 public class Spellpanel : MonoBehaviour {
 
+    private SpellManager spellManager;
+    private EventManager eventManager;
+
     private List<Element> _spell = new List<Element>();
 
 
 	// Use this for initialization
 	void Start () {
-	    
-	}
+        spellManager = GameObject.Find("Managers").GetComponent<SpellManager>();
+        eventManager = GameObject.Find("Managers").GetComponent<EventManager>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,6 +36,17 @@ public class Spellpanel : MonoBehaviour {
     }
     void SendSpell()
     {
-
+        Debug.Log("SendSpell");
+        if (_spell.Count > 0)
+        {
+            Spell spell = spellManager.FindSpell(_spell);
+            if (spell != null)
+            {
+                Debug.Log("Spell: "+ spell.name);
+                eventManager.SpellCasted(spell);
+            }
+            else
+                Debug.Log("Spell: Null");
+        }
     }
 }
