e047ad3f8891ce733a42a9dbb53c52fc7ce11c66
Bas Roding <bas.roding@gmail.com>
2016-01-31 00:17:54 +0100

    Added nice scene

---
+++ GGJ2016/Assets/Scripts/PlingManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlingManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject plingPrefab;
+
+    public void ShowPlings(int amount, Enemy enemy){
+        for(int i = 0; i < amount; i++){
+            GameObject go = Instantiate(plingPrefab);
+            go.transform.SetParent(transform, false);
+            Vector2 viewportPoint = Camera.main.WorldToViewportPoint(enemy.transform.position);
+            go.GetComponent<RectTransform>().anchorMin = viewportPoint;
+            go.GetComponent<RectTransform>().anchorMax = viewportPoint;
+
+            float percentageAmount = PlayerManager.instance.PercentageAmount;
+
+            LeanTween.move(go.GetComponent<RectTransform>(), new Vector3(100 * percentageAmount, 0, -200), 1.2f).setEase(LeanTweenType.easeInBack);
+        }
+    }
+}
