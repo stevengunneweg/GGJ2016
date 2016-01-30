d027674a620821a0bfb53df93c3d912a234fbb84
Goosfrabix <goosfraba05@hotmail.com>
2016-01-30 16:11:06 +0100

    Cooldown timer

--- GGJ2016/Assets/Scripts/UI/Spelitem.cs
+++ GGJ2016/Assets/Scripts/UI/Spelitem.cs
@@ -6,17 +6,21 @@ public class Spelitem : MonoBehaviour {
     public Element element;
     private Spellpanel _spellPanel;
     Button _button;
+    Text _text;
 	// Use this for initialization
 	void Start () {
         _spellPanel = transform.parent.GetComponent<Spellpanel>();
         GetComponent<Image>().sprite = element.Image;
         _button = GetComponent<Button>();
+        _text = transform.Find("Text").GetComponent<Text>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         _button.interactable = element.Available;
-	}
+        _text.text = element.Available ? "" : Mathf.Ceil(element.CurrentCooldown).ToString();
+
+    }
     public void OnClick()
     {
         _spellPanel.AddElement(element);
