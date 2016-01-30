5b1e506334b58f32f39d7ee6aa775caa09a3aed2
Gunnaway <stevengunneweg@gmail.com>
2016-01-30 12:55:34 +0100

    added effects for all spells

---
+++ GGJ2016/Assets/Scripts/SpellEffects/EarthSpellEffect.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpellEffect : BaseSpellEffect {
+	public override void ApplyEffectToEnemy(Enemy enemy) {
+		enemy.Kill();
+	}
+}
---
+++ GGJ2016/Assets/Scripts/SpellEffects/LightningSpellEffect.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningSpellEffect : BaseSpellEffect {
+	public override void ApplyEffectToEnemy(Enemy enemy) {
+		enemy.Kill();
+	}
+}
