68836b5c773d8b733869ccdea89f25bd6a26b398
madnessxd <brotherhoodofnod201@hotmail.com>
2016-01-30 16:25:45 +0100

    Temple sounds.

--- GGJ2016/Assets/Scripts/Temple.cs
+++ GGJ2016/Assets/Scripts/Temple.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class Temple : MonoBehaviour {
 
     public void RaiseTemple(){
-        LeanTween.moveLocalY(gameObject, transform.localPosition.y + 1.6f, 1).setEase(LeanTweenType.easeOutQuad);
-        ShakeCamera();
+		LeanTween.moveLocalY(gameObject, transform.localPosition.y + 1.6f, 1).setEase(LeanTweenType.easeOutQuad);
+		ShakeCamera();
+		Sound sound = new Sound (transform.root.gameObject.GetComponent<AudioSource> (), "SFX/" + "TempleGrow");
     }
 
     public void LowerTemple(){
-        LeanTween.moveLocalY(gameObject, transform.localPosition.y - 1.6f, 1).setEase(LeanTweenType.easeOutQuad);
-        ShakeCamera();
+		LeanTween.moveLocalY(gameObject, transform.localPosition.y - 1.6f, 1).setEase(LeanTweenType.easeOutQuad);
+		ShakeCamera();
+		Sound sound = new Sound (transform.root.gameObject.GetComponent<AudioSource> (), "SFX/" + "TempleShrink");
     }
 
     private void ShakeCamera(){
