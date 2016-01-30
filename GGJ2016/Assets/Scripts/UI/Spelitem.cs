db556cbd2a704f018c9fff12229d93105fc02a11
Goosfrabix <goosfraba05@hotmail.com>
2016-01-30 15:55:02 +0100

    Spelitem disable when cooldown

--- GGJ2016/Assets/Scripts/UI/Spelitem.cs
+++ GGJ2016/Assets/Scripts/UI/Spelitem.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 public class Spelitem : MonoBehaviour {
     public Element element;
     private Spellpanel _spellPanel;
+    Button _button;
 	// Use this for initialization
 	void Start () {
         _spellPanel = transform.parent.GetComponent<Spellpanel>();
         GetComponent<Image>().sprite = element.Image;
+        _button = GetComponent<Button>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        _button.interactable = element.Available;
 	}
     public void OnClick()
     {
