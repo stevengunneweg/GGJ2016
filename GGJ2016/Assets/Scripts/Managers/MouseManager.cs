1ccdd56976eb9366761351b5410bf646adef62b9
madnessxd <brotherhoodofnod201@hotmail.com>
2016-01-31 01:58:31 +0100

    MouseManager init

---
+++ GGJ2016/Assets/Scripts/Managers/MouseManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+	void OnMouseEnter() {
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+	void OnMouseExit() {
+		Cursor.SetCursor(null, Vector2.zero, cursorMode);
+	}
+}
