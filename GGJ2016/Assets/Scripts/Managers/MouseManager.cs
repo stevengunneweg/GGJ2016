295bb9a7cc4c89fc3021b5f4d8693c49383fbf22
madnessxd <brotherhoodofnod201@hotmail.com>
2016-01-31 02:08:33 +0100

    Muis #SWEK

--- GGJ2016/Assets/Scripts/Managers/MouseManager.cs
+++ GGJ2016/Assets/Scripts/Managers/MouseManager.cs
@@ -6,9 +6,13 @@ public class MouseManager : MonoBehaviour {
 	public CursorMode cursorMode = CursorMode.Auto;
 	public Vector2 hotSpot = Vector2.zero;
 	void OnMouseEnter() {
-		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+		//Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
 	}
 	void OnMouseExit() {
 		Cursor.SetCursor(null, Vector2.zero, cursorMode);
+	}
+
+	void Start(){
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
 	}
 }
