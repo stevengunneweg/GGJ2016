a9428a8b071abc7f2e6800e44e03ad88d24a6fad
Bas Roding <bas.roding@gmail.com>
2016-01-31 11:48:33 +0100

    Added menu's

---
+++ GGJ2016/Assets/GoToNextScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(NextRoutine());
+	}
+
+    private IEnumerator NextRoutine(){
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Menu");
+    }
+}
