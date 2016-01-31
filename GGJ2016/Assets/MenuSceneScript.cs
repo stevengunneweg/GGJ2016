0a9725ecd4595cbfc0b30b295acd8a94ab7c9bac
Goosfrabix <goosfraba05@hotmail.com>
2016-01-31 14:52:03 +0100

    free mode with * in keypad in MenuSceneScript

--- GGJ2016/Assets/MenuSceneScript.cs
+++ GGJ2016/Assets/MenuSceneScript.cs
@@ -7,4 +7,9 @@ public class MenuSceneScript : MonoBehaviour {
     public void Play(){
         SceneManager.LoadScene("Main");
     }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.KeypadMultiply))
+            PlayerManager.FreeMode = !PlayerManager.FreeMode;
+    }
 }
