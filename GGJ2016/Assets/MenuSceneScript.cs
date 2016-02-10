3daa2423d001bcea628f68ca865b310a5db2bd53
Goosfrabix <goosfraba05@hotmail.com>
2016-02-11 00:38:10 +0100

    Show a godmode indicator in  Menu

--- GGJ2016/Assets/MenuSceneScript.cs
+++ GGJ2016/Assets/MenuSceneScript.cs
@@ -4,12 +4,32 @@ using UnityEngine.SceneManagement;
 
 public class MenuSceneScript : MonoBehaviour {
 
+    RectTransform godmodeNote;
+    float targetx = 260;
+    float inpos;
+    float outpos;
+
+    void Start()
+    {
+        godmodeNote = transform.Find("GodModeNote").GetComponent<RectTransform>();
+        targetx = inpos = godmodeNote.localPosition.x;
+        outpos = inpos - 240;
+    }
+
     public void Play(){
         SceneManager.LoadScene("Main");
     }
     void Update()
     {
-        if (Input.GetKey(KeyCode.KeypadMultiply))
+        if (Input.GetKeyDown(KeyCode.KeypadMultiply))
             PlayerManager.FreeMode = !PlayerManager.FreeMode;
+
+        LerpNote();
+
+    }
+    void LerpNote()
+    {
+        targetx = PlayerManager.FreeMode ? outpos : inpos;
+        godmodeNote.localPosition += (new Vector3(targetx, godmodeNote.localPosition.y, godmodeNote.localPosition.z)- godmodeNote.localPosition) * (Time.deltaTime* 20);
     }
 }
