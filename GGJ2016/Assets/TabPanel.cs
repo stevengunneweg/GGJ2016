cb827288936f952daf432dd5d7520d0c6ed29504
Goosfrabix <goosfraba05@hotmail.com>
2016-02-10 22:26:26 +0100

    buttons now clickable still dirty as fuck, but thats the way i like it. Also they have numbers showing which key to press

--- GGJ2016/Assets/TabPanel.cs
+++ GGJ2016/Assets/TabPanel.cs
@@ -12,28 +12,48 @@ public class TabPanel : MonoBehaviour {
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            spellManager.AddElementToQueue(spellManager.Elements[0]);
-            transform.GetChild(0).GetComponent<Tab>().Select();
-            transform.GetChild(1).GetComponent<Tab>().Deselect();
-            transform.GetChild(2).GetComponent<Tab>().Deselect();
+            SelectSpell(0);
         } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            spellManager.AddElementToQueue(spellManager.Elements[1]);
-            transform.GetChild(0).GetComponent<Tab>().Deselect();
-            transform.GetChild(1).GetComponent<Tab>().Select();
-            transform.GetChild(2).GetComponent<Tab>().Deselect();
+            SelectSpell(1);
         } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            spellManager.AddElementToQueue(spellManager.Elements[2]);
-            transform.GetChild(0).GetComponent<Tab>().Deselect();
-            transform.GetChild(1).GetComponent<Tab>().Deselect();
-            transform.GetChild(2).GetComponent<Tab>().Select();
+            SelectSpell(2);
         } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
             spellManager.AddElementToQueue(spellManager.Elements[3]);
         }
     }
+    private void SelectSpell(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                spellManager.AddElementToQueue(spellManager.Elements[0]);
+                transform.GetChild(0).GetComponent<Tab>().Select();
+                transform.GetChild(1).GetComponent<Tab>().Deselect();
+                transform.GetChild(2).GetComponent<Tab>().Deselect();
+                break;
+            case 1:
+                spellManager.AddElementToQueue(spellManager.Elements[1]);
+                transform.GetChild(0).GetComponent<Tab>().Deselect();
+                transform.GetChild(1).GetComponent<Tab>().Select();
+                transform.GetChild(2).GetComponent<Tab>().Deselect();
+                break;
+            case 2:
+                spellManager.AddElementToQueue(spellManager.Elements[2]);
+                transform.GetChild(0).GetComponent<Tab>().Deselect();
+                transform.GetChild(1).GetComponent<Tab>().Deselect();
+                transform.GetChild(2).GetComponent<Tab>().Select();
+                break;
+        }
+    }
+
 
     public void DeselectAll(){
         transform.GetChild(0).GetComponent<Tab>().Deselect();
         transform.GetChild(1).GetComponent<Tab>().Deselect();
         transform.GetChild(2).GetComponent<Tab>().Deselect();
+    }
+    public void ClickElement(int elementID)
+    {
+        SelectSpell(elementID);
     }
 }
