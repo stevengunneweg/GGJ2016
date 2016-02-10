6637622751d2be6ccb7a7ad56796ee003ded3b4f
Goosfrabix <goosfraba05@hotmail.com>
2016-02-11 00:59:22 +0100

    fancy camera movement

--- GGJ2016/Assets/MenuSceneScript.cs
+++ GGJ2016/Assets/MenuSceneScript.cs
@@ -9,8 +9,16 @@ public class MenuSceneScript : MonoBehaviour {
     float inpos;
     float outpos;
 
+    Transform _camera;
+    Vector3 lookatTarget;
+    Vector3 startposition;
+    Vector3 targetposition;
+
     void Start()
     {
+        _camera = GameObject.Find("Main Camera").GetComponent<Transform>();
+        startposition = _camera.position;
+        lookatTarget = _camera.position+(_camera.forward*2);
         godmodeNote = transform.Find("GodModeNote").GetComponent<RectTransform>();
         targetx = inpos = godmodeNote.localPosition.x;
         outpos = inpos - 240;
@@ -26,10 +34,16 @@ public class MenuSceneScript : MonoBehaviour {
 
         LerpNote();
 
+        _camera.LookAt(lookatTarget);
+
     }
     void LerpNote()
     {
         targetx = PlayerManager.FreeMode ? outpos : inpos;
         godmodeNote.localPosition += (new Vector3(targetx, godmodeNote.localPosition.y, godmodeNote.localPosition.z)- godmodeNote.localPosition) * (Time.deltaTime* 20);
+        
+        targetposition = PlayerManager.FreeMode ? new Vector3(startposition.x, startposition.y - 0.5f, startposition.z) : startposition;
+        _camera.position += (targetposition - _camera.position) * (Time.deltaTime * 20);
+
     }
 }
